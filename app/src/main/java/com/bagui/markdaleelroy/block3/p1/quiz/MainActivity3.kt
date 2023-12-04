c40b9840fc11c572ebcc8316f499c5074b892cee
Dale <markdalebagui@gmail.com>
2023-12-04 21:14:22 +0800

    100%

--- app/src/main/java/com/bagui/markdaleelroy/block3/p1/quiz/MainActivity3.kt
+++ app/src/main/java/com/bagui/markdaleelroy/block3/p1/quiz/MainActivity3.kt
@@ -2,10 +2,21 @@ package com.bagui.markdaleelroy.block3.p1.quiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity3 : AppCompatActivity() {
+    private lateinit var tvTitle: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main3)
+        setContentView(R.layout.activity_main2)
+
+        tvTitle = findViewById(R.id.tv_title)
+        val getFirst = intent.getStringExtra("first")
+        val getSecond = intent.getStringExtra("middle")
+        val getThird = intent.getStringExtra("last")
+
+        tvTitle.text= "$getFirst$getSecond$getThird"
+
     }
+
 }
