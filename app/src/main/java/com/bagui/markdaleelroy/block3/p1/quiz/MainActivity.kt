34c040e91e2dae7a8e7f661ffa095526387b444d
Dale <markdalebagui@gmail.com>
2023-12-04 21:02:51 +0800

    Initial commit

---
+++ app/src/main/java/com/bagui/markdaleelroy/block3/p1/quiz/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.bagui.markdaleelroy.block3.p1.quiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
