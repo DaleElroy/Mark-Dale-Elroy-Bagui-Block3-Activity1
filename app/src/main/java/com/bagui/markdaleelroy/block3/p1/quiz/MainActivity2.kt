8c3ca4292b64a4dd9c183a71ca675b11d99abe8e
Dale <markdalebagui@gmail.com>
2023-12-04 21:13:31 +0800

    Function 2

--- app/src/main/java/com/bagui/markdaleelroy/block3/p1/quiz/MainActivity2.kt
+++ app/src/main/java/com/bagui/markdaleelroy/block3/p1/quiz/MainActivity2.kt
@@ -1,11 +1,24 @@
 package com.bagui.markdaleelroy.block3.p1.quiz
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity2 : AppCompatActivity() {
+    private lateinit var tvTitle: TextView
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+        tvTitle = findViewById(R.id.tv_title)
+        val getFirst = intent.getStringExtra("last")
+        val getSecond = intent.getStringExtra("first")
+        val getThird = intent.getStringExtra("middle")
+
+        tvTitle.text= "$getFirst$getSecond$getThird"
+
     }
+
 }
